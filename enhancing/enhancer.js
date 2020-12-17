27e57ed77a8bbcec742912d1d2a12c875b8e686f
Tatiana Efimova <tatka_ef@mail.ru>
2020-12-16 23:58:03 -0800

    methods

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,27 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  if (item.enhancement !== 20) {
+    return { ...item, enhancement: item.enhancement + 1  }; 
+  } else {
+    return { ...item };
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement > 16) {
+    return {...item, enhancement: item.enhancement - 1}
+  } 
+  if (item.enhancement > 15) {
+  return { ...item, durability: item.durability - 10}
+  }
+  if (item.enhancement < 15) {
+    return { ...item, durability: item.durability -5}
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,13 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('Sanity and intro', () => {
+    it('works', () => { // THIS is the test
+      // assertions
+      expect(2 + 2).toBe(4)
+    })
+    test('everything working', () => { // THIS is another test
+      expect({}).not.toBe({})
+      expect({}).toEqual({})
+    })
+  })
