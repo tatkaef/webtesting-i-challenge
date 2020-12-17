38327fb8475a1102c4d05f5c4af217d774fc6153
Tatiana Efimova <tatka_ef@mail.ru>
2020-12-17 01:01:17 -0800

    MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -13,16 +13,17 @@ function success(item) {
   }
 }
 
-function fail(item) {
-  if (item.enhancement > 16) {
-    return {...item, enhancement: item.enhancement - 1}
-  } 
-  if (item.enhancement > 15) {
-  return { ...item, durability: item.durability - 10}
-  }
+function fail(item) {  
   if (item.enhancement < 15) {
     return { ...item, durability: item.durability -5}
   }
+  if (item.enhancement > 16) {
+    return {...item, enhancement: item.enhancement - 1, durability: item.durability - 10}
+  } 
+  if (item.enhancement >= 15) {
+    return { ...item, durability: item.durability - 10}
+  }
+  
 }
 
 function repair(item) {
